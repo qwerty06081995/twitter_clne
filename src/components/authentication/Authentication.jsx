d7c5c9252b59e418af84a316ad5a9a2eb91c3c91
nurzhan <nurzhanduisenbaev480@mail.ru>
2026-03-27 02:52:13 +0500

    Redux, Axios

--- src/components/authentication/Authentication.jsx
+++ src/components/authentication/Authentication.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import {Grid} from "@mui/material";
 
 const Authentication = () => {
     return (
-        <div>Authentication</div>
+        <div>
+            <Grid className={"overflow-y-hidden"} container>
+                <Grid className={"hidden lg:block"} size={{lg:7}}>
+                    <img
+                        className={"w-full h-screen"}
+                        src="https://i.playground.ru/p/vtrj9pzLan4heZouzc9E6A.jpeg"
+                        alt=""/>
+                    <div className={"absolute top-[26%] left-[19%]"}>
+                        <svg
+                            height={300}
+                            width={300}
+                            viewBox="0 0 24 24"
+                            aria-hidden="true"
+                            className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-18jsvk2 r-rxcuwo r-1777fci r-m327ed r-494qqr">
+                            <g>
+                                <path d="M21.742 21.75l-7.563-11.179 7.056-8.321h-2.456l-5.691 6.714-4.54-6.714H2.359l7.29 10.776L2.25 21.75h2.456l6.035-7.118 4.818 7.118h6.191-.008zM7.739 3.818L18.81 20.182h-2.447L5.29 3.818h2.447z"></path>
+                            </g>
+                        </svg>
+                    </div>
+                </Grid>
+                <Grid className={""}>
+                {/*Building A Twitter Full Stack Clone Using Spring Boot, React, MySQL, MUI Tailwind and Formik 28:29*/}
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
